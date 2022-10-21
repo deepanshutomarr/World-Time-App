d3296804e4f57c57465625adccf5363972a8b026
Deepanshu <tomardeepanshu12@gmail.com>
2022-10-21 11:48:48 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter_application/pages/choose_location.dart';
 import 'package:flutter_application/pages/home.dart';
 import 'package:flutter_application/pages/loading.dart';
 
-void main() => runApp(MaterialApp(
-
-      debugShowCheckedModeBanner: false,
-
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Loading(),
-        '/home': (context) => const Home(),
-        '/location': (context) => const ChooseLocation(),
-      },
-    ));
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const Loading(),
+          '/home': (context) => const Home(),
+          '/location': (context) => const ChooseLocation(),
+        },
+      ),
+    );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -86,3 +86,4 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
